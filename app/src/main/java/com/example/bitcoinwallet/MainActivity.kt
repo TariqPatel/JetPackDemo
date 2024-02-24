679c557699433d222344901353b8f66fce37b4bb
Tariq Patel <tariqpatel9@gmail.com>
2024-02-24 19:26:18 +0200

    Add a viewmodel

--- app/src/main/java/com/example/bitcoinwallet/MainActivity.kt
+++ app/src/main/java/com/example/bitcoinwallet/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.bitcoinwallet.ui.theme.BitcoinWalletTheme
+import androidx.compose.runtime.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyComposable()
                 }
             }
         }
@@ -42,5 +53,25 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     BitcoinWalletTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun MyComposable(viewModel: MyViewModel = viewModel()) {
+    val dataList by viewModel.dataList
+
+    LazyColumn {
+        itemsIndexed(dataList) { index, item ->
+            Text(text = item, modifier = Modifier.padding(16.dp))
+        }
+    }
+}
+
+class MyViewModel : ViewModel() {
+    private val _dataList = mutableStateOf(listOf("Item 1", "Item 2"))
+    val dataList: State<List<String>> = _dataList
+
+    fun updateDataList(newList: List<String>) {
+        _dataList.value = newList
     }
 }
