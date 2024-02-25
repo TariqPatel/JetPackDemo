992b12f52f1c88252f234e0ce2d2fc68884245d4
Tariq Patel <tariqpatel9@gmail.com>
2024-02-25 07:12:51 +0200

    style view some more

--- app/src/main/java/com/example/bitcoinwallet/MainActivity.kt
+++ app/src/main/java/com/example/bitcoinwallet/MainActivity.kt
@@ -10,12 +10,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -29,6 +31,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -104,20 +107,36 @@ fun CurrencyConverterView(viewModel: MyViewModel = viewModel()) {
         ) {
             Text("Fetch values for your BTC")
         }
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.Gray)
-        ) {
-            itemsIndexed(currencyList) { index, currency ->
-                Text(
-                    text = "Currency: ${currency.currencyName}," +
-                            " Value: ${currency.currencyValue}",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp),
-                    color = Color.White
-                )
+        if (viewModel.currencyList.value.isEmpty()) {
+            Text(
+                text = "Your Bitcoin value for different currencies will be shown here",
+                color = Color.Black,
+                style = MaterialTheme.typography.displaySmall,
+                textAlign = TextAlign.Center
+            )
+        } else {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Magenta)
+            ) {
+                itemsIndexed(currencyList) { index, currency ->
+                    Text(
+                        text = "Currency: ${currency.currencyName}\n" +
+                                "Value: ${currency.currencyValue}",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        color = Color.White,
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                    Divider(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(2.dp)
+                            .background(MaterialTheme.colorScheme.onBackground.copy(alpha = 0.1f))
+                    )
+                }
             }
         }
     }
